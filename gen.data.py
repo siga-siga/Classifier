68924f08304ef6c8cb98a3ffb734c3607cedccfd
siga-siga <73140978+siga-siga@users.noreply.github.com>
2021-03-18 18:23:14 +0900

    画像データをNumPy配列形式へ変換

---
+++ gen.data.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import os, glob
+import numpy as np
+from sklearn import model_selection
+
+classes = ["Oldman","Sparrow", "Straycat"]
+num_classes = len(classes)
+image_size = 50 #画像リサイズ
+
+#画像の読みこみ
+
+X = []
+Y = []
+for index, classlabel in enumerate(classes):
+    photos_dir = "./" + classlabel
+    files = glob.glob(photos_dir + "/*.jpg")
+    for i, file in enumerate(files):
+        if i >= 200: break
+        image = Image.open(file)
+        image = image.convert("RGB")
+        image = image.resize((image_size, image_size))
+        data = np.asarray(image)
+        X.append(data)
+        Y.append(index)
+
+X = np.array(X)
+Y = np.array(Y)
